ecd3e71de5aa8622a51cc372a0bb4efa264cfafa
jisoo <doh_0112@naver.com>
2023-04-25 22:07:54 +0900

    button border 추가와 switch문 수정

--- src/components/Btns.jsx
+++ src/components/Btns.jsx
@@ -7,16 +7,19 @@ const StContainer = styled.div`
   text-align: center;
 `;
 
-const StBtn = styled.div`
+const StBtn = styled.button`
   width: ${(props) => props.width};
   height: ${(props) => props.height};
-  line-height: ${(props) => props.height};
+  line-height: calc(${(props) => props.height} - 5px);
   background-color: ${(props) => props.backgroundColor};
   margin: 3px 5px;
-  border: none;
+  border: ${(props) => props.border};
   cursor: pointer;
   border-radius: 8px;
+  box-sizing: border-box;
   color: ${(props) => props.color};
+  font-weight: 600;
+  font-size: 13.3px;
 `;
 
 // 색을 넣으면, 이름을 반환해주는 함수를 만듭니다.
@@ -25,7 +28,7 @@ const getBoxName = (box) => {
   if (box.name === "BtnForModal") return "open modal";
   switch (box.width) {
     case "200px":
-      return "Large Primary Button";
+      return box.border === "3px solid rgb(85, 239, 196)" ? "Large Primary Button" : "Large Negative Button";
     case "130px":
       return "mideum";
     case "100px":
@@ -38,12 +41,20 @@ const Btns = ({ btnList, handleClickSave }) => {
   return (
     <StContainer>
       {btnList.map((box) =>
-        box.backgroundColor === "rgb(85, 239, 196)" ? (
-          <StBtn onClick={handleClickSave} height={box.height} width={box.width} backgroundColor={box.backgroundColor} color={box.color} key={box.id}>
+        box.backgroundColor === "rgb(85, 239, 196)" || box.border === "3px solid rgb(85, 239, 196)" ? (
+          <StBtn
+            onClick={handleClickSave}
+            height={box.height}
+            width={box.width}
+            backgroundColor={box.backgroundColor}
+            color={box.color}
+            border={box.border}
+            key={box.id}
+          >
             {getBoxName(box)}
           </StBtn>
         ) : (
-          <StBtn height={box.height} width={box.width} backgroundColor={box.backgroundColor} color={box.color} key={box.id}>
+          <StBtn height={box.height} width={box.width} backgroundColor={box.backgroundColor} border={box.border} color={box.color} key={box.id}>
             {getBoxName(box)}
           </StBtn>
         )
