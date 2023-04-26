cb340757906fe83a4d3784ca46170fbfea0b4df7
jisoo <doh_0112@naver.com>
2023-04-27 03:05:18 +0900

    p태그 추가

--- src/App.js
+++ src/App.js
@@ -11,30 +11,46 @@ const StBox = styled.div`
   margin: 10px;
 `;
 
+const StTxt = styled.p`
+  font-size: 32px;
+  font-weight: 600;
+  margin: 21.4px 0;
+`;
+
+const StConModal = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 function App() {
   // button list
   const greenBtnList = [
-    { width: "200px", height: "50px", backgroundColor: "rgb(85, 239, 196)", color: "rgb(0, 0, 0)", id: 1 },
-    { width: "130px", height: "45px", backgroundColor: "rgb(85, 239, 196)", color: "rgb(0, 0, 0)", id: 2 },
-    { width: "100px", height: "40px", backgroundColor: "rgb(85, 239, 196)", color: "rgb(0, 0, 0)", id: 3 },
+    { width: "200px", height: "50px", backgroundColor: "rgb(255, 255, 255)", color: "rgb(0, 0, 0)", id: 1, border: "3px solid rgb(85, 239, 196)" },
+    { width: "130px", height: "45px", backgroundColor: "rgb(85, 239, 196)", color: "rgb(0, 0, 0)", id: 2, border: "none" },
+    { width: "100px", height: "40px", backgroundColor: "rgb(85, 239, 196)", color: "rgb(0, 0, 0)", id: 3, border: "none" },
   ];
   const PinkBtnList = [
-    { width: "200px", height: "50px", backgroundColor: "rgb(250, 177, 160)", color: "rgb(214, 48, 49)", id: 4 },
-    { width: "130px", height: "45px", backgroundColor: "rgb(250, 177, 160)", color: "rgb(214, 48, 49)", id: 5 },
-    { width: "100px", height: "40px", backgroundColor: "rgb(250, 177, 160)", color: "rgb(214, 48, 49)", id: 6 },
+    { width: "200px", height: "50px", backgroundColor: "rgb(255, 255, 255)", color: "rgb(214, 48, 49)", id: 4, border: "3px solid rgb(250, 177, 160)" },
+    { width: "130px", height: "45px", backgroundColor: "rgb(250, 177, 160)", color: "rgb(214, 48, 49)", id: 5, border: "none" },
+    { width: "100px", height: "40px", backgroundColor: "rgb(250, 177, 160)", color: "rgb(214, 48, 49)", id: 6, border: "none" },
   ];
 
-  const smallBtnList = [{ width: "100px", height: "40px", backgroundColor: "rgb(85, 239, 196)", color: "rgb(0, 0, 0)", name: "BtnForInp", id: 7 }];
+  const smallBtnList = [
+    { width: "100px", height: "40px", backgroundColor: "rgb(85, 239, 196)", color: "rgb(0, 0, 0)", name: "BtnForInp", id: 7, border: "none" },
+  ];
 
   return (
     <StBox className="App">
-      <p>Button</p>
+      <StTxt>Button</StTxt>
       <Btns btnList={greenBtnList} />
       <Btns btnList={PinkBtnList} />
+      <StTxt>input</StTxt>
       <Inputs smallBtnList={smallBtnList} />
-      <p>Modal</p>
-      <Modal modalType={"modal_1"} />
-      <Modal modalType={"modal_2"} />
+      <StTxt>Modal</StTxt>
+      <StConModal>
+        <Modal modalType={"modal_1"} />
+        <Modal modalType={"modal_2"} />
+      </StConModal>
       <Select />
     </StBox>
   );
